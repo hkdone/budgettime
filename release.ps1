efd8fec5b080b1fab7ad53d261a969a3041bea37
hkdone <jean.virassa@gmail.com>
2026-02-18 20:41:12 +0100

    Official Release v1.0.0

--- release.ps1
+++ release.ps1
@@ -23,7 +23,7 @@ Write-Host "2. Updating config.yaml..."
 Write-Host "3. Building Flutter Web..."
 flutter clean
 flutter pub get
-flutter build web --web-renderer html --release
+flutter build web --release
 
 if ($LASTEXITCODE -ne 0) {
     Write-Error "Build failed!"
