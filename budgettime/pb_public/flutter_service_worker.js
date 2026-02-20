a58fd2edc0627855e8bd42da935717dd9db51e1e
hkdone <jean.virassa@gmail.com>
2026-02-20 20:04:14 +0100

    Feat: Add recurrence editing and deletion + unified release workflow

--- budgettime/pb_public/flutter_service_worker.js
+++ budgettime/pb_public/flutter_service_worker.js
@@ -25,16 +25,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a9121a3135181f47e0be707091bac3ad",
+"flutter_bootstrap.js": "8a025905e82f31f343e10ac846202968",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "3e5071e971dfb7ff874938caf5f6b4f3",
 "/": "3e5071e971dfb7ff874938caf5f6b4f3",
-"main.dart.js": "674b20a6ef1a30701545b4b516fcf676",
+"main.dart.js": "996faec814ab23a4d6d7496bd69d8efa",
 "manifest.json": "a6c25ab6b4115eb85ede9de16d98294d",
-"version.json": "f9569536049bd92276c3ade8c9988280"};
+"version.json": "7c293dca05a9720362911a59dd826df3"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
