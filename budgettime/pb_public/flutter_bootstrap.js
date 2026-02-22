182c6eb7e50da6fb00ba4f05ca65ad1baf7b0390
hkdone <jean.virassa@gmail.com>
2026-02-22 21:16:27 +0100

    Stable Release v1.8.9 - Robust date fix and final cleanup

--- budgettime/pb_public/flutter_bootstrap.js
+++ budgettime/pb_public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "956244237"
+    serviceWorkerVersion: "142011528"
   }
 });
--- budgettime/pb_public/flutter_service_worker.js
+++ budgettime/pb_public/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "429e08bc684e3f9f74ee09e18ac831ea",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d7665c3164393fc394ca55c2581daf3b",
+"flutter_bootstrap.js": "15585586bed5cf2ddfdf20e259b63efb",
 "icons/Icon-192.png": "429e08bc684e3f9f74ee09e18ac831ea",
 "icons/Icon-512.png": "429e08bc684e3f9f74ee09e18ac831ea",
 "icons/Icon-maskable-192.png": "429e08bc684e3f9f74ee09e18ac831ea",
