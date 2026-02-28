2638cbc1b22a071389ded80285808603d5bbc3ec
hkdone <jean.virassa@gmail.com>
2026-02-28 08:10:37 +0100

    Mise Ã  jour version 1.9.71

--- budgettime/pb_public/flutter_service_worker.js
+++ budgettime/pb_public/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "429e08bc684e3f9f74ee09e18ac831ea",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "05920ccb99487c13476abb322b4c5607",
+"flutter_bootstrap.js": "42e21fa222722ac2bdeaa6b2350bfd5f",
 "icons/Icon-192.png": "429e08bc684e3f9f74ee09e18ac831ea",
 "icons/Icon-512.png": "429e08bc684e3f9f74ee09e18ac831ea",
 "icons/Icon-maskable-192.png": "429e08bc684e3f9f74ee09e18ac831ea",
