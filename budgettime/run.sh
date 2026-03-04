868e180d1e3a353e9386ee7ae3bd9856996d7506
hkdone <jean.virassa@gmail.com>
2026-03-04 06:40:25 +0100

    fix(docker): Change directory before starting pocketbase to resolve missing publicDir flag issue

--- budgettime/run.sh
+++ budgettime/run.sh
@@ -8,11 +8,11 @@ ls -lh /pb/pocketbase
 if [ -d "/data" ]; then
     echo "Environment: Home Assistant Add-on"
     echo "Data will be stored in /share/budgettime for user visibility"
-    # On pointe vers le dossier share pour que l'utilisateur puisse le voir/vider
     mkdir -p /share/budgettime
-    exec /pb/pocketbase serve --http=0.0.0.0:8090 --dir="/share/budgettime/pb_data" --publicDir="/pb/pb_public"
+    cd /share/budgettime
+    exec /pb/pocketbase serve --http=0.0.0.0:8090 --dir="pb_data"
 else
     echo "Environment: Docker Standard"
-    # On pointe vers le volume monté standard
-    exec /pb/pocketbase serve --http=0.0.0.0:8090 --dir="/pb/pb_data" --publicDir="/pb/pb_public"
+    cd /pb
+    exec /pb/pocketbase serve --http=0.0.0.0:8090 --dir="pb_data"
 fi
